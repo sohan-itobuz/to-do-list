5f52acc0898329477e6ab263c71dfd9fc9d073f2
sohan-itobuz <sohan@itobuz.com>
2025-10-10 18:53:34 +0530

    filter added and folder structure change start

--- src/js/main.js
+++ src/js/main.js
@@ -45,6 +45,8 @@ const todoAPI = {
         },
         body: JSON.stringify(task),
       });
+      loadTasks();
+
       if (!response.ok) throw new Error("Failed to create task");
       return await response.json();
     } catch (error) {
@@ -63,6 +65,7 @@ const todoAPI = {
         },
         body: JSON.stringify(updates),
       });
+      loadTasks();
       if (!response.ok) throw new Error("Failed to update task");
       return await response.json();
     } catch (error) {
@@ -118,7 +121,7 @@ function createTaskElement(task) {
   const li = document.createElement("li");
   li.className =
     "list-group-item d-md-flex  justify-content-between flex-column border border-danger";
-  li.dataset.id = task.id;
+  li.dataset.id = task._id; //_id
 
   if (task.completed) {
     li.classList.add("task-completed");
@@ -137,6 +140,7 @@ function createTaskElement(task) {
       break;
   }
 
+  // _id
   li.innerHTML = `
       
       <div class="d-flex align-items-center justify-content-center justify-content-md-between ">
@@ -148,14 +152,14 @@ function createTaskElement(task) {
     }">
         <span class="${priorityClass} ">${task.priority === 3 ? "High" : task.priority === 2 ? "Medium" : "Low"
     }</span>
-        </div>
-        <div class="btn-group gap-2 ms-2">
-          <button class="btn  btn-sm edit-btn rounded-1" data-id="${task.id
+        </div> 
+        <div class="btn-group gap-2 ms-2"> 
+          <button class="btn  btn-sm edit-btn rounded-1" data-id="${task._id
     }"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" style="pointer-events: none;">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
           </svg></button>
-          <button class="btn  btn-sm delete-btn rounded-1" data-id="${task.id
+          <button class="btn  btn-sm delete-btn rounded-1" data-id="${task._id
     }"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16" style="pointer-events: none;">
           <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
           </svg></button>
@@ -166,42 +170,41 @@ function createTaskElement(task) {
   return li;
 }
 
-function sortAndRenderTasks() {
-  // const sortedTasks = [...tasks].sort((a, b) => {
-  //   if (a.completed && !b.completed) return 1;
-  //   if (!a.completed && b.completed) return -1;
-  //   if (!a.completed && !b.completed) {
-  //     return b.priority - a.priority;
-  //   }
-  //   return 0;
-  // });
+// function sortAndRenderTasks() {
+//   // const sortedTasks = [...tasks].sort((a, b) => {
+//   //   if (a.completed && !b.completed) return 1;
+//   //   if (!a.completed && b.completed) return -1;
+//   //   if (!a.completed && !b.completed) {
+//   //     return b.priority - a.priority;
+//   //   }
+//   //   return 0;
+//   // });
 
-  todoList.innerHTML = "";
+//   todoList.innerHTML = "";
 
-  if (!tasks.length) {
-    const emptyMessage = document.createElement("li");
-    emptyMessage.className =
-      "list-group-item text-center p-3 text-muted border-0 bg-transparent";
-    emptyMessage.textContent = "No tasks found. Start adding or searching!";
-    todoList.appendChild(emptyMessage);
-  }
+//   if (!tasks.length) {
+//     const emptyMessage = document.createElement("li");
+//     emptyMessage.className =
+//       "list-group-item text-center p-3 text-muted border-0 bg-transparent";
+//     emptyMessage.textContent = "No tasks found. Start adding or searching!";
+//     todoList.appendChild(emptyMessage);
+//   }
 
-  sortedTasks.forEach((task) => {
-    if (!task.tags) {
-      task.tags = [];
-    }
-    todoList.appendChild(createTaskElement(task));
-  });
-}
+//   sortedTasks.forEach((task) => {
+//     if (!task.tags) {
+//       task.tags = [];
+//     }
+//     todoList.appendChild(createTaskElement(task));
+//   });
+// }
 
 const sortAndRenderTasksSearch = () => {
-  tasks.sort((a, b) => {
-    if (a.completed !== b.completed) {
-      return a.completed ? 1 : -1;
-    }
-    return b.priority - a.priority;
-  });
-
+  // tasks.sort((a, b) => {
+  //   if (a.completed !== b.completed) {
+  //     return a.completed ? 1 : -1;
+  //   }
+  //   return b.priority - a.priority;
+  // });
   todoList.innerHTML = "";
   if (!tasks.length) {
     const emptyMessage = document.createElement("li");
@@ -216,6 +219,15 @@ const sortAndRenderTasksSearch = () => {
   }
 };
 
+async function loadTasks() {
+  try {
+    tasks = await todoAPI.getAllTasks();
+    sortAndRenderTasksSearch();
+  } catch (error) {
+    console.error("Failed to load tasks:", error);
+  }
+}
+
 const loadTasksSearch = async (searchTerm = "", searchCategory = "") => {
   try {
     const fetchedTasks = await todoAPI.getAllTasks(
@@ -228,6 +240,8 @@ const loadTasksSearch = async (searchTerm = "", searchCategory = "") => {
     todoList.innerHTML = '<div class="alert alert-danger text-center" role="alert">Failed to load tasks. Please check the server connection.</div>';
   }
 };
+
+// Event Listeners 
 
 const searchForm = document.getElementById("search-form");
 if (searchForm) {
@@ -251,7 +265,37 @@ if (searchTermInput) {
   });
 }
 
-// Event Listeners 
+
+document.getElementById("all-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  loadTasksSearch("", "");
+  // document.getElementById("search-term").value = "";
+  // document.getElementById("search-category").value = "";
+});
+document.getElementById("high-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  loadTasksSearch("3", "priority");
+  // document.getElementById("search-term").value = "";
+  // document.getElementById("search-category").value = "";
+});
+document.getElementById("mid-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  loadTasksSearch("2", "priority");
+  // document.getElementById("search-term").value = "";
+  // document.getElementById("search-category").value = "";
+});
+document.getElementById("low-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  loadTasksSearch("1", "priority");
+  // document.getElementById("search-term").value = "";
+  // document.getElementById("search-category").value = "";
+});
+document.getElementById("completed-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  loadTasksSearch("true", "completed");
+  // document.getElementById("search-term").value = "";
+  // document.getElementById("search-category").value = "";
+});
 
 document
   .getElementById("todo-form")
@@ -277,7 +321,7 @@ document
         });
 
         tasks.push(newTask);
-        sortAndRenderTasks();
+        sortAndRenderTasksSearch(); //updated
         taskInput.value = "";
         if (tagsInput) tagsInput.value = "";
       } catch (error) {
@@ -344,7 +388,7 @@ todoList.addEventListener("change", async function (event) {
 async function updateTask(taskId, updates) {
   try {
     const updatedTask = await todoAPI.updateTask(taskId, updates);
-    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+    const taskIndex = tasks.findIndex((task) => task._id === taskId); //_id
     if (taskIndex !== -1) {
       tasks[taskIndex] = { ...tasks[taskIndex], ...updatedTask };
       sortAndRenderTasksSearch();
@@ -373,7 +417,8 @@ document
     try {
       await todoAPI.deleteTask(taskId);
       tasks = tasks.filter((task) => task.id !== taskId);
-      sortAndRenderTasks();
+      sortAndRenderTasksSearch(); //updated
+      loadTasks(); //to refresh the list after deletion
 
       const deleteConfirmationModal = bootstrap.Modal.getInstance(
         document.getElementById("deleteConfirmationModal")
@@ -393,7 +438,7 @@ document
     try {
       await todoAPI.deleteAllTasks();
       tasks = [];
-      sortAndRenderTasks();
+      sortAndRenderTasksSearch(); //updated
 
       const deleteAllConfirmationModal = bootstrap.Modal.getInstance(
         document.getElementById("deleteAllConfirmationModal")
