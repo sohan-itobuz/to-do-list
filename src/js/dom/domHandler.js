72b34ecef37acb799e96f549cfbe6fa2e3b1352d
sohanChatterjee7 <sohanchatterjee017@gmail.com>
2025-10-26 00:09:52 +0530

    file modulation wip

--- src/js/dom/domHandler.js
+++ src/js/dom/domHandler.js
@@ -1,68 +1,75 @@
 export const todoMain = {
   todoList: document.getElementById("todo-list"),
   searchForm: document.getElementById("search-form"),
-  searchTermInput: document.getElementById("search-term"),
+  searchTerm: document.getElementById("search-term"),
+  searchCategory: document.getElementById("search-category"),
   allBtn: document.getElementById("all-btn"),
   highBtn: document.getElementById("high-btn"),
   midBtn: document.getElementById("mid-btn"),
   lowBtn: document.getElementById("low-btn"),
   completeBtn: document.getElementById("completed-btn"),
   todoForm: document.getElementById("todo-form"),
+  todoInput: document.getElementById("todo-input"),
+  todoPriority: document.getElementById("priority-input"),
+  tagsInput: document.getElementById("tags-input"),
 
   delAllModal: document.getElementById("clear-all-btn"),
   delBtn: document.getElementById("confirmDeleteBtn"),
   delAllBtn: document.getElementById("confirmDeleteAllBtn"),
-}
+
+  delConfirmModal: document.getElementById("deleteConfirmationModal"),
+  delAllConfirmModal: document.getElementById("deleteAllConfirmationModal"),
+};
 
 export const forgotPasswordPage = {
-  forgotPassForm: document.querySelector('form'),
-  emailInput: document.getElementById('email-input'),
+  forgotPassForm: document.querySelector("form"),
+  emailInput: document.getElementById("email-input"),
   submitButton: document.querySelector('button[type="submit"]'),
-}
+};
 
 export const forgotOtpVerify = {
-  inputs: document.querySelectorAll('#otp > *[id]'),
+  inputs: document.querySelectorAll("#otp > *[id]"),
   otpForm: document.getElementById("otp-form"),
-  otpInputs: document.querySelectorAll('#otp input'),
-  validateButton: document.querySelector('.validate'),
-  resendOtp: document.getElementById('resend-otp'),
-}
+  otpInputs: document.querySelectorAll("#otp input"),
+  validateButton: document.querySelector(".validate"),
+  resendOtp: document.getElementById("resend-otp"),
+};
 
 export const forgotPasswordReset = {
   form: document.getElementById("reset-form"),
   passwordInput: document.getElementById("newPassword"),
-}
+};
 
 export const login = {
-  loginForm: document.querySelector('form'),
-  emailInput: document.getElementById('email-input'),
-  passwordInput: document.getElementById('password-input'),
+  loginForm: document.querySelector("form"),
+  emailInput: document.getElementById("email-input"),
+  passwordInput: document.getElementById("password-input"),
   submitButton: document.querySelector('button[type="submit"]'),
-}
+};
 
 export const otpSelector = {
-  inputs: document.querySelectorAll('#otp > *[id]'),
-  otpInputs: document.querySelectorAll('#otp input'),
-  validateButton: document.querySelector('.validate'),
-  resendLink: document.querySelector('.resend-link'),
-  resendOtp: document.getElementById('resend-otp'),
-}
+  inputs: document.querySelectorAll("#otp > *[id]"),
+  otpInputs: document.querySelectorAll("#otp input"),
+  validateButton: document.querySelector(".validate"),
+  resendLink: document.querySelector(".resend-link"),
+  resendOtp: document.getElementById("resend-otp"),
+};
 
 export const resetPassword = {
   form: document.getElementById("resetPasswordForm"),
   emailInput: document.getElementById("emailInput"),
   oldPasswordInput: document.getElementById("oldPasswordInput"),
   newPasswordInput: document.getElementById("newPasswordInput"),
-}
+};
 
 export const signUp = {
-  signupForm: document.getElementById('signup-form'),
-  emailInput: document.getElementById('email'),
-  passwordInput: document.getElementById('password'),
-  submitButton: document.getElementById('submit-btn'),
-}
+  signupForm: document.getElementById("signup-form"),
+  emailInput: document.getElementById("email"),
+  passwordInput: document.getElementById("password"),
+  submitButton: document.getElementById("submit-btn"),
+};
 
 export const profile = {
-  emailSpan: document.getElementById('user-email'),
-  logoutBtn: document.getElementById('logout-btn'),
-}
+  emailSpan: document.getElementById("user-email"),
+  logoutBtn: document.getElementById("logout-btn"),
+};
--- src/js/events/events.js
+++ src/js/events/events.js
@@ -1,26 +1,22 @@
-// Import our custom CSS
-// import "../scss/styles.scss";
-
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
-import { renderTodos } from '../utils/utils.js';
-import { updateTask } from '../utils/utils.js';
 import { todoMain } from "../dom/domHandler.js";
-import TodoApi from '../api/TodoApi.js';
-const todoAPI = new TodoApi();
 
 import EventHandlers from "./EventHandlers.js";
 
 export function initializeEventHandlers(todoList) {
-
   // Search form
   if (todoMain.searchForm) {
-    todoMain.searchForm.addEventListener("submit", EventHandlers.searchFormHandle);
+    todoMain.searchForm.addEventListener(
+      "submit",
+      EventHandlers.searchFormHandle
+    );
   }
 
   // Search term input
-  if (todoMain.searchTermInput) {
-    todoMain.searchTermInput.addEventListener("input", EventHandlers.searchInputHandle);
+  if (todoMain.searchTerm) {
+    todoMain.searchTerm.addEventListener(
+      "input",
+      EventHandlers.searchInputHandle
+    );
   }
 
   // Filter buttons
@@ -41,71 +37,33 @@ export function initializeEventHandlers(todoList) {
   }
 
   if (todoMain.completeBtn) {
-    todoMain.completeBtn.addEventListener("click", EventHandlers.handleCompleteBtn);
+    todoMain.completeBtn.addEventListener(
+      "click",
+      EventHandlers.handleCompleteBtn
+    );
   }
 
-  // create task event 
+  // create task event
   if (todoMain.todoForm) {
-    todoMain.todoForm.addEventListener("submit", EventHandlers.handleCreateTodo);
+    todoMain.todoForm.addEventListener(
+      "submit",
+      EventHandlers.handleCreateTodo
+    );
   }
 
   // Task list events
-  todoList.addEventListener("click", function (event) {
-    const target = event.target;
-    const li = target.closest("li");
-
-    if (target.classList.contains("delete-btn")) {
-      const taskId = li.dataset.id;
-      const taskText = li.querySelector(".task-text").textContent;
-
-      document.getElementById("taskToDeleteText").textContent = taskText;
-      document.getElementById("confirmDeleteBtn").dataset.id = taskId;
-
-      const deleteConfirmationModal = new bootstrap.Modal(
-        document.getElementById("deleteConfirmationModal")
-      );
-      deleteConfirmationModal.show();
-    }
-
-    if (target.classList.contains("edit-btn")) {
-      const taskId = li.dataset.id;
-      const taskTextSpan = li.querySelector(".task-text");
-      const editInput = li.querySelector(".edit-input");
-
-      if (taskTextSpan.style.display !== "none") {
-        taskTextSpan.style.display = "none";
-        editInput.style.display = "block";
-        editInput.focus();
-        target.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16" style="pointer-events: none;"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.243 8.381a.733.733 0 0 1 1.066-.01L7.293 10.98l5.17-5.17z"/></svg>`;
-      } else {
-        const updatedText = editInput.value.trim();
-        if (updatedText) {
-          updateTask(taskId, { title: updatedText });
-        }
-        taskTextSpan.style.display = "block";
-        editInput.style.display = "none";
-        target.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" style="pointer-events: none;">
-          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-        </svg>`;
-      }
-    }
-  });
-
-
-
+  todoList.addEventListener("click", EventHandlers.handleDelEdtBtn);
 
   if (todoList) {
     todoList.addEventListener("change", EventHandlers.handleChecked);
-
   }
-
-
 
   if (todoMain.delAllModal) {
-    todoMain.delAllModal.addEventListener("click", EventHandlers.handleDelAllModal);
+    todoMain.delAllModal.addEventListener(
+      "click",
+      EventHandlers.handleDelAllModal
+    );
   }
-
 
   // Delete confirmation
   if (todoMain.delBtn) {
@@ -116,5 +74,4 @@ export function initializeEventHandlers(todoList) {
   if (todoMain.delAllBtn) {
     todoMain.delAllBtn.addEventListener("click", EventHandlers.handleDelAllBtn);
   }
-
 }
